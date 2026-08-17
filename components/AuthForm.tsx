7fb42897a4c67cf31bc5aa60468f3bc13e3ace19
tronzard <satyamkhatiwada1999@gmail.com>
2026-08-17 13:15:36 +0545

    components addition and form schema init

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,52 @@
+"use client";
+
 import React from "react";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroupTextarea,
+} from "@/components/ui/input-group";
 
 type FormType = "sign-in" | "sign-up";
 
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(5, "Bug title must be at least 5 characters.")
+    .max(32, "Bug title must be at most 32 characters."),
+});
+
 const AuthForm = ({ type }: { type: FormType }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
   return <div>AuthForm</div>;
 };
 
