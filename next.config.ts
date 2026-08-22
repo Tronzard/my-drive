27fd0a055b57a245d9c258cf328b86b4dbbf80a5
tronzard <satyamkhatiwada1999@gmail.com>
2026-08-22 14:24:00 +0545

    added mobile navigation side drawer

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "png.pngtree.com",
+      },
+      {
+        protocol: "https",
         hostname: "cloud.appwrite.io",
       },
     ],
